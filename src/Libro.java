dcb004b78eb92e05dd5b13b79109021ff14ebd33
juanXxue6 <juanxue6@gmail.com>
2022-10-07 10:17:32 +0200

    commit libro

---
+++ src/Libro.java
@@ -0,0 +1,78 @@
+
+public class Libro {
+
+	private int isbn;
+	private String titulo;
+	private String autor;
+	private int numeroPaginas;
+
+	
+	public Libro(int isbn, String titulo, String autor, int numeroPaginas) {
+		
+		ComprobarISBN(numeroPaginas);
+		
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.numeroPaginas = numeroPaginas;
+	}
+
+	public int GetIsbn() {
+		return isbn;
+	}
+	
+	public void SetIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	
+	
+	public String GetTitulo() {
+		return titulo;
+	}
+	
+	
+	public void SetTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+	public String GetAutor() {
+		return autor;
+	}
+	
+	
+	public void SetAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	
+	public int GetNumeroPaginas() {
+		return numeroPaginas;
+	}
+	
+	
+	public void SetNumeroPaginas(int numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+	
+	public void ToString() {
+		
+		System.out.println("El " + titulo + " con " + isbn +
+				" creado por el " + autor + " tiene " + numeroPaginas);
+	}
+	
+	
+	private void ComprobarISBN(int isbn) {
+		
+		if(String.valueOf(isbn).length() != 10 && String.valueOf(isbn).length() != 13) {
+			
+			System.out.println("Longitud del ISBN no valido, introduzca uno valido");
+		}
+		
+		
+	}
+	
+	
+	
+
+}
